188d02d5bfea10f5ec95b486d03c75d6e06fff95
moshkin <moshkin@finch-melrose.com>
2016-08-08 18:07:21 +0300

    filter

--- app/components/Filter/index.js
+++ app/components/Filter/index.js
@@ -1,21 +1,45 @@
 import React, { PropTypes } from 'react';
 import style from './Filter.css';
+import img from './search.png'
 
-const Filter = (props) => {
-  return (
-    <div className={style.filter}>
-      <div className={style.input}>
-        <input type="text" placeholder="input"/>
+export default class Filter extends React.Component {
+
+  state = {
+    filterShown: false
+  };
+
+  toggleFilter = () => {
+    this.setState({
+      filterShown: !this.state.filterShown
+    })
+  };
+
+  render() {
+    return (
+      <div className={style.filter}>
+        <img className={style.filterSearch} src={img} alt="" title="" onClick={this.toggleFilter}/>
+        {
+          this.state.filterShown
+          &&
+          <div className={style.filterFields}>
+            <div className={style.input}>
+              <input type="text" placeholder="input"/>
+            </div>
+            <div className={style.input}>
+              <input type="text" placeholder="input"/>
+            </div>
+            <div className={style.input}>
+              <select>
+                <option>select</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+              </select>
+            </div>
+          </div>
+        }
       </div>
-      <div className={style.input}>
-        <input type="text" placeholder="input"/>
-      </div>
-      <div className={style.input}>
-        <input type="text" placeholder="input"/>
-      </div>
-    </div>
-  )
+    )
+  }
+
 };
-
-
-export default Filter
